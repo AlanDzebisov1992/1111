320815be784ac1e761b144bb7bfe7fec481174bc
nik <alan1992dzebis15rus@gmail.com>
2023-04-02 18:35:46 +0300

    урок 6 while i for

---
+++ домашка while i for.py
@@ -0,0 +1,39 @@
+
+
+
+
+# урок 6 задание 1
+
+# n = int(input())
+# cnt = 0
+
+# for i in range(0, n):
+#    num = int(input())
+#    if num == 0:
+#        cnt += 1
+# print(cnt)
+
+
+# задание 2
+
+# x = int(input())
+# if x == 1:
+#    d = 1
+# else:
+#    d = 2
+#    for i in range(2, int((x / 2)) + 1):
+#        if x % i == 0:
+#           d += 1
+# print(d)
+
+# задание 3 
+
+a = int(input())
+b = int(input())
+
+for i in range(a, b, 2):
+    if a <= b:
+        print()
+    else:
+        i % 2 == 0
+    print(i)
